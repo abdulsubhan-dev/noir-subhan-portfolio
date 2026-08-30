0fa7bbedb9fa39cd671e145470a4a486f398bcf7
Abdul Subhan <abdulsubhan.design@gmail.com>
2026-08-30 12:56:56 +0500

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx,js,jsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        noir: '#0C0C0C',
+        pale: '#D7E2EA'
+      }
+    }
+  },
+  plugins: []
+}
